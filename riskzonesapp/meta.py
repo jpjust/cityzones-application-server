fe4c4d188017198f3218d06f518bb691ea921023
João Paulo Just Peixoto <just1982@gmail.com>
2023-01-04 11:46:48 +0000

    Refactor .env.example and meta for database use

--- riskzonesapp/meta.py
+++ riskzonesapp/meta.py
@@ -6,82 +6,74 @@ riskzones background app.
 '''
 
 from datetime import datetime
-import json
 import os
 
-# Create the queue and output directories
-try:
-  os.makedirs(os.getenv('QUEUE_DIR'))
-  os.makedirs(os.getenv('OUTPUT_DIR'))
-except FileExistsError:
-  pass
-
 def make_polygon(polygon: list) -> dict:
-  '''
-  Generate a GeoJSON structure for the polygon.
-  '''
-  pol_dict = {
-    "type": "FeatureCollection",
-    "name": "meta",
-    "crs": {
-      "type": "name",
-      "properties": {
-        "name": "urn:ogc:def:crs:EPSG::4674"
-      }
-    },
-    "features": [
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "MultiPolygon",
-          "coordinates": [[]]
-        }
-      }
-    ]
-  }
+    '''
+    Generate a GeoJSON structure for the polygon.
+    '''
+    pol_dict = {
+        "type": "FeatureCollection",
+        "name": "meta",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:EPSG::4674"
+            }
+        },
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "MultiPolygon",
+                    "coordinates": [[]]
+                }
+            }
+        ]
+    }
 
-  # Add each point to the GeoJSON structure
-  for point in polygon[:-1]:
-    pol_dict['features'][0]['geometry']['coordinates'][0].append(point)
+    # Add each point to the GeoJSON structure
+    for point in polygon[:-1]:
+        pol_dict['features'][0]['geometry']['coordinates'][0].append(point)
 
-  return pol_dict
+    return pol_dict
 
 def make_config_file(polygon: list, zl: int) -> tuple:
-  '''
-  Generate a JSON configuration for the riskzones rool.
-  '''
-  timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
-  base_filename = f"task_{timestamp}"
+    '''
+    Generate a JSON configuration for the riskzones rool.
+    '''
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
+    base_filename = f"task_{timestamp}"
 
-  # Calculate AoI boundaries
-  left = right = polygon[0][0]
-  top = bottom = polygon[0][1]
+    # Calculate AoI boundaries
+    left = right = polygon[0][0]
+    top = bottom = polygon[0][1]
 
-  for point in polygon[1:]:
-    if point[0] < left:   left   = point[0]
-    if point[0] > right:  right  = point[0]
-    if point[1] < bottom: bottom = point[1]
-    if point[1] < top:    top    = point[1]
-  
-  base_conf = {
-    "left": left,
-    "bottom": bottom,
-    "right": right,
-    "top": top,
-    "zone_size": zl,
-    "cache_zones": True,
-    "M": int(os.getenv('RZ_M')),
-    "edus": int(os.getenv('RZ_EDUS')),
-    "geojson": f"{os.getenv('QUEUE_DIR')}/{base_filename}.geojson",
-    "pois": f"{os.getenv('QUEUE_DIR')}/{base_filename}.osm",
-    "pois_types": {
-      "amenity": [],
-      "railway": []
-    },
-    "edu_alg": "restricted",
-    "output": f"{os.getenv('OUTPUT_DIR')}/{base_filename}_map.csv",
-    "output_edus": f"{os.getenv('OUTPUT_DIR')}/{base_filename}_edus.csv",
-    "output_roads": f"{os.getenv('OUTPUT_DIR')}/{base_filename}_roads.csv",
-  }
+    for point in polygon[1:]:
+        if point[0] < left:   left   = point[0]
+        if point[0] > right:  right  = point[0]
+        if point[1] < bottom: bottom = point[1]
+        if point[1] < top:    top    = point[1]
+    
+    base_conf = {
+        "left": left,
+        "bottom": bottom,
+        "right": right,
+        "top": top,
+        "zone_size": zl,
+        "cache_zones": True,
+        "M": int(os.getenv('RZ_M')),
+        "edus": int(os.getenv('RZ_EDUS')),
+        "geojson": f"{os.getenv('QUEUE_DIR')}/{base_filename}.geojson",
+        "pois": f"{os.getenv('QUEUE_DIR')}/{base_filename}.osm",
+        "pois_types": {
+            "amenity": [],
+            "railway": []
+        },
+        "edu_alg": "restricted",
+        "output": f"{os.getenv('OUTPUT_DIR')}/{base_filename}_map.csv",
+        "output_edus": f"{os.getenv('OUTPUT_DIR')}/{base_filename}_edus.csv",
+        "output_roads": f"{os.getenv('OUTPUT_DIR')}/{base_filename}_roads.csv",
+    }
 
-  return base_filename, base_conf
+    return base_filename, base_conf
